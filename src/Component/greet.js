5ef1df47da0174d54624feaf897a70ab999b30aa
shubhangi67 <shubhangis.4567@gmail.com>
2025-07-27 23:10:41 +0530

    First Commit Types of Component

---
+++ src/Component/greet.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+// function Greet()
+// {
+//     return <h1>Hello, from Shubhangi!</h1>;
+// }
+const Greet = () => <h1>Hello, From Shubhangi</h1>
+
+export default Greet
